b7292e13286c0077498fbcd90c8467b0cb2be6ec
TianqiLyu <Tianqilyu96@gmail.com>
2021-10-22 19:24:59 +1100

    set up static page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div>APP...</div>;
+    return (
+      <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header"><h2>React Router Demo</h2></div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            <a className="list-group-item active" href="./about.html">About</a>
+            <a className="list-group-item" href="./home.html">Home</a>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              ????
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
   }
 }
